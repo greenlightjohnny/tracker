674bb2a7681289a9764f01f6ebba7eb3f19820fb
fj <castelloiv@gmail.com>
2020-10-08 18:08:37 -0400

    Hmmm

--- controllers/transcontroller.js
+++ controllers/transcontroller.js
@@ -1,8 +1,22 @@
 ///all the methods used to interact with the database
 
-//@description: Get all transactions
-// @route GET /api/v1/transactions
+// @des    Get all transactions
+// @route  GET /api/v1/transactions
 // @access Public
 exports.getTransactions = (req, res, next) => {
   res.send("GET transactions");
+};
+
+// @des    Add transaction
+// @route  POST /api/v1/transactions
+// @access Public
+exports.addTransaction = (req, res, next) => {
+  res.send("POST transactions");
+};
+
+// @des    DELETE transaction
+// @route  DELETE /api/v1/transactions/:id
+// @access Public
+exports.deleteTransaction = (req, res, next) => {
+  res.send("Delete transaction");
 };
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,14 +1,20 @@
 const express = require("express");
 
 const router = express.Router();
-const { getTransactions } = require("../controllers/transcontroller");
+const {
+  getTransactions,
+  addTransaction,
+  deleteTransaction,
+} = require("../controllers/transcontroller");
 
 // sends requests to the transactionController module, for GET
 //router.get("/", getTransactions); changed to router.route()
 
 // Remember, the '/' is actually referring to /api/v1/transactions. It was linked here in the server.js file as"
 // app.use("/api/v1/transactions", transactions);
-// YOu can attach methods to the route below
-router.route("/").get(getTransactions);
+// YOu can attach methods to the route below, calls the function called getTransactions in the /transcontroller module.
+router.route("/").get(getTransactions).post(addTransaction);
+
+router.route("/:id").delete(deleteTransaction);
 
 module.exports = router;
